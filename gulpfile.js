010cbd5b5402372d398e02e2183d7132ef65e5c0
Ilya Lakhin <eliah.lakhin@gmail.com>
2014-01-26 19:24:57 +0700

    Browserify configured

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
+var debug = require('gulp-debug');
 var watch = require('gulp-watch');
+var rename = require('gulp-rename');
 
 var server = require('tiny-lr')();
 
@@ -8,35 +10,34 @@ gulp.task('default', ['compile', 'live']);
 gulp.task('compile', ['jade', 'coffee']);
 
 gulp.task('jade', function() {
-  gulp
-    .src('./src/**/*.jade')
-    .pipe(watch())
+  watch({glob: './src/**/*.jade'})
     .pipe(require('gulp-jade')({pretty: true}))
-    .pipe(gulp.dest('./output/live'));
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('coffee', function() {
-  gulp
-    .src('./src/**/*.coffee')
-    .pipe(watch())
-    .pipe(require('gulp-coffee')())
-    .pipe(gulp.dest('./output/live'));
+  watch({glob: './src/**/*.coffee'}, function() {
+    gulp
+      .src('./src/index.coffee', {read: false})
+      .pipe(require('gulp-browserify')({
+        transform: ['coffeeify'],
+        extensions: ['.coffee']
+      }))
+      .pipe(rename('index.js'))
+      .pipe(gulp.dest('./build'));
+  });
 });
 
 gulp.task('live', ['server'], function() {
   server.listen(35729, function(err) {
     if (err) throw err;
 
-    gulp
-      .src('./output/live/**/*')
-      .pipe(watch())
+    watch({glob: './build/**/*'})
       .pipe(require('gulp-livereload')(server))
   });
 });
 
 gulp.task('server', ['compile'], require('gulp-serve')({
-  root: './output/live',
+  root: './build',
   middleware: require('connect-livereload')()
 }));
-
-gulp.task('')
